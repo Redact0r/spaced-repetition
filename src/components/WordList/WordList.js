bf0ed2b4e80761ef5f535a02ecc448db9da01347
Ren F <renfinn89@gmail.com>
2020-06-23 09:00:44 -0700

    wordlist

--- src/components/WordList/WordList.js
+++ src/components/WordList/WordList.js
@@ -1,12 +1,25 @@
-import React, { Component } from "react";
-import UserContext from "../../contexts/UserContext";
+import React, { Component } from 'react';
+import UserContext from '../../contexts/UserContext';
 
 class WordList extends Component {
   static contextType = UserContext;
 
-    renderWords() {return this.context.words.map((word) => <p>{word.original}</p>} );
-    render() {
-        return (<div className="worldlist-div">{this.words}</div>);
+  renderWords() {
+    return this.context.words.map((word) => <p>{word.original}</p>);
+  }
+
+  render() {
+    return (
+      <div className="worldlist-div">
+        {this.context.words.length === 0 ? (
+          <div className="list">
+            <p>No Words found for this language</p>
+          </div>
+        ) : (
+          <div className="list">{this.renderWords()}</div>
+        )}
+      </div>
+    );
   }
 }
 
