805e1e0f99f34810e7379e53025f649acacda3d5
Ren F <renfinn89@gmail.com>
2020-06-25 08:00:10 -0700

    started correct/incorrect promt

--- src/components/LearningForm/LearningForm.js
+++ src/components/LearningForm/LearningForm.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import { Label, Input } from '../Form/Form';
 import Button from '../Button/Button';
+import Word from '../Word/Word';
+import UserService from '../../services/user-service';
 
 class LearningForm extends Component {
   state = {
     guess: '',
+    correct: '',
+    incorrect: '',
+    check: 0
   };
-  handleSubmit = (e) => {};
+
+  guessCheck = () => {
+    guess.toLowercase()
+if(guess === word.translation){
+check++
+correct++
+}
+else{
+  incorrect++
+  check-1
+}
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+this.guessCheck;
+UserService.
+  };
 
   firstInput = React.createRef();
   render() {
     console.log(this.state);
     return (
+      <div>
       <form>
         <Label htmlFor="learn-guess-input">
           What's the translation for this word?
@@ -30,6 +52,8 @@ class LearningForm extends Component {
           Submit your answer
         </Button>
       </form>
+      {this.state.check > 0 ? (<Correct />):(<Incorrect />)}
+      </div>
     );
   }
 }
